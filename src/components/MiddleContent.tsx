77a7e8699585e7eab9ed057ff082b88cba6ba425
Lie DING <dingliefr123@163.com>
2023-02-11 23:52:48 +0000

    feat: completed most parts

--- src/components/MiddleContent.tsx
+++ src/components/MiddleContent.tsx
@@ -1,77 +1,160 @@
-import React, {  useContext } from "react"
-import type { ProjectModel } from "../models/ProjectModel"
-import { GLOBAL_CONTEXT } from "../reducer"
-import { Affiche, LeftBarEnum } from "../typing"
+import React, { useContext, useMemo } from "react";
+import { ProjectModel, ProjectStatusEnum } from "../models/ProjectModel";
+import { GLOBAL_CONTEXT, ACTION_TYPE_ENUM } from "../reducer";
+import { Affiche, LeftBarEnum } from "../typing";
+import { CircleMinnus, CirclePlus, SearchIcon } from "../icons";
+import styles from "./index.module.scss";
 
 interface IMiddleContent {
-  projets: ProjectModel[],
-  affiches: Affiche[]
+  projets: ProjectModel[];
+  affiches: Affiche[];
 }
+
+const Search: React.FC = () => {
+  return (
+    <div className={styles.search}>
+      <input />
+      <SearchIcon />
+    </div>
+  );
+};
 
 const RowWithTitleAndOperBtn: React.FC<{
-  title: string
-}> = ({ title }) => <div>
-  <span>+  -</span>
-  <span>{ title }</span>
-</div>
+  title: string;
+  dim?: boolean;
+  selected?: boolean;
+  clickHandler: () => void;
+  children?: React.ReactElement | null;
+}> = ({ title, dim, selected, clickHandler, children }) => (
+  <div
+    className={`${dim ? "dim-text" : ""} ${styles["row-with-title"]}`}
+    onClick={clickHandler}
+  >
+    {dim ? (
+      <span className={styles["span-placceholder"]}></span>
+    ) : selected ? (
+      <CircleMinnus />
+    ) : (
+      <CirclePlus />
+    )}
+    <span
+      className={[
+        styles["row-with-title-span"],
+        selected ? "selected" : "",
+      ].join(" ")}
+    >
+      {title}
+    </span>
+    {children ?? null}
+  </div>
+);
 
 const ProjetList: React.FC<{
-  projets: ProjectModel[],
-  selectedProjIds: string[]
-}> = ({ projets, selectedProjIds }) => <div>
-  {
-    projets.map((item) =>
-      <RowWithTitleAndOperBtn
-        key={item.id}
-        title={item.name}
-      />
-    )
-  }
-</div>
+  projets: ProjectModel[];
+  selectedProjIds: string[];
+  selectHandler: (id: string) => void;
+}> = ({ projets, selectedProjIds, selectHandler }) => {
+  return (
+    <div>
+      {projets.map((item) => {
+        const isArchived = item.status === ProjectStatusEnum.ARCHIVED;
+        const clickHandler = () => selectHandler(item.id);
+        return (
+          <RowWithTitleAndOperBtn
+            key={item.id}
+            title={item.name}
+            dim={isArchived}
+            clickHandler={clickHandler}
+            selected={selectedProjIds.includes(item.id)}
+          >
+            {isArchived ? <span className="dim-text">(Archived)</span> : null}
+          </RowWithTitleAndOperBtn>
+        );
+      })}
+    </div>
+  );
+};
 
 const AfficheList: React.FC<{
-  affiches: Affiche[],
-  selectedAffcihIds: number[]
-}> = ({ affiches, selectedAffcihIds }) => <div>
-  {
-    affiches.map((item) =>
-      <RowWithTitleAndOperBtn
-        key={item.id}
-        title={item.name}
-      />
-    )
+  affiches: Affiche[];
+  selectedAffairIds: number[];
+  selectHandler: (id: number) => void;
+}> = ({ affiches, selectedAffairIds, selectHandler }) => {
+  const projetsIncludingAffiches = useMemo(() => {
+    const map: Record<string, Affiche[]> = {};
+    for (const affich of affiches) {
+      const pName = affich.pName;
+      if (pName in map) map[pName].push(affich);
+      else map[pName] = [affich];
+    }
+    return Object.values(map);
+  }, [affiches]);
+
+  return (
+    <div>
+      {projetsIncludingAffiches.map((affiches) => {
+        const { pName } = affiches[0];
+        return (
+          <React.Fragment key={pName}>
+            <h4>{pName}</h4>
+            {affiches.map((item) => {
+              const clickHandler = () => selectHandler(item.id);
+              return (
+                <RowWithTitleAndOperBtn
+                  key={item.id}
+                  title={item.name}
+                  clickHandler={clickHandler}
+                  selected={selectedAffairIds.includes(item.id)}
+                />
+              );
+            })}
+          </React.Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+const ListWrapper: React.FC<
+  IMiddleContent & {
+    ProjetListRender: typeof ProjetList;
+    AffichesRender: typeof AfficheList;
   }
-</div>
+> = ({ ProjetListRender, projets, affiches }) => {
+  const { state, dispatch } = useContext(GLOBAL_CONTEXT);
+  const selectHandler = (type: ACTION_TYPE_ENUM) => (id: number | string) => {
+    dispatch({ type, payload: id });
+  };
 
-const ListWrapper: React.FC<IMiddleContent & {
-  ProjetListRender: typeof ProjetList,
-  AffichesRender: typeof AfficheList
-}> = ({
-  ProjetListRender,
-  projets
-}) => {
-  const { state } = useContext(GLOBAL_CONTEXT);
-  //const clickHandler = () => {};
-  
   if (state.activeBarKey === LeftBarEnum.PROJETS)
-    return <ProjetListRender
-      projets={projets}
-      selectedProjIds={state.selectedProjetIds || []} 
-    />
+    return (
+      <ProjetListRender
+        projets={projets}
+        selectedProjIds={state.selectedProjetIds || []}
+        selectHandler={selectHandler(ACTION_TYPE_ENUM.TOGGLE_SELECT_PROJET)}
+      />
+    );
 
-  return null
-}
+  return (
+    <AfficheList
+      affiches={affiches || []}
+      selectedAffairIds={state.selectedAffairIds || []}
+      selectHandler={selectHandler(ACTION_TYPE_ENUM.TOGGLR_SELECT_AFFAIRE)}
+    />
+  );
+};
 
 const MiddleContent: React.FC<IMiddleContent> = (props) => {
+  return (
+    <div>
+      <Search />
+      <ListWrapper
+        {...props}
+        ProjetListRender={ProjetList}
+        AffichesRender={AfficheList}
+      />
+    </div>
+  );
+};
 
-  return <div>
-    <div>Search</div>
-    <ListWrapper
-      {...props}
-      ProjetListRender={ProjetList}
-      AffichesRender={AfficheList}
-    />
-  </div>
-}
-
-export default MiddleContent
+export default MiddleContent;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,24 +1,23 @@
-import Data from '../data/data';
-import { ProjectStatusEnum } from '../models/ProjectModel';
-import { Affiche } from '../typing';
+import Data from "../data/data";
+import { ProjectStatusEnum } from "../models/ProjectModel";
+import { Affiche } from "../typing";
 
-type StrOrNum = string | number
-export function delItemInArrById<K extends StrOrNum>(
-  arr: K[],
-  id: K
-) {
-  const idx = arr.indexOf(id);
-  if (idx < 0) return arr;
-  arr.splice(idx, 1)
-  return [ ...arr ]
-}
+// type StrOrNum = string | number;
 
-export const DEL_EXCLUDEDE_DATA = Data
-  .filter(item => item.status !== ProjectStatusEnum.DELETED);
+export const DEL_EXCLUDEDE_DATA = Data.filter(
+  (item) => item.status !== ProjectStatusEnum.DELETED
+);
 
-export const AFFICHES = DEL_EXCLUDEDE_DATA
-  .map(({ name: pName, affairs }) =>
-    affairs?.map(affair => ({ ...affair, pName }))
-  )
+export const AFFICHES = DEL_EXCLUDEDE_DATA.map(({ name: pName, affairs }) =>
+  affairs?.map((affair) => ({ ...affair, pName }))
+)
   .flat(2)
   .filter(Boolean) as Affiche[];
+
+export function toggleIdInArr<K>(arr: K[], id: K) {
+  if (!arr || !Array.isArray(arr)) return [];
+  const idx = arr.indexOf(id);
+  if (idx < 0) return [...arr, id];
+  arr.splice(idx, 1);
+  return arr.slice();
+}
